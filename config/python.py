0108f223244b09127d718d201f2d18efba8d9dd0
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 10:09:58 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,28 +1,20 @@
-""" python depedencies for this project """
-from typing import List
+""" python deps for this project """
 
-
-console_scripts: List[str] = [
+console_scripts: list[str] = [
     "pybookmarks=pybookmarks.main:main",
 ]
-dev_requires: List[str] = [
-    "pypitools",
-    "black",
-]
-config_requires: List[str] = [
+config_requires: list[str] = [
     "pyclassifiers",
 ]
-install_requires: List[str] = []
-build_requires: List[str] = [
+build_requires: list[str] = [
     "pymakehelper",
     "pydmt",
 ]
-test_requires: List[str] = [
+test_requires: list[str] = [
     "pytest",
     "pytest-cov",
-    "flake8",
     "pylint",
     "mypy",
     "types-requests",
 ]
-requires = config_requires + install_requires + build_requires + test_requires
+requires = config_requires + build_requires + test_requires
