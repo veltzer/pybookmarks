dca79139be096395064700c5e73015abed96e7b5
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 09:33:32 +0300

    fixed up deps

--- pybookmarks/static.py
+++
@@ -1,5 +0,0 @@
-""" version which can be consumed from within the module """
-VERSION_STR = "0.0.2"
-DESCRIPTION = "Book mark manager and sync between browsers"
-APP_NAME = "pybookmarks"
-LOGGER_NAME = "pybookmarks"
