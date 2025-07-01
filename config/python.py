1fff2ceb15e960ee513e8c4bb59cd46746edb9bf
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 15:55:14 +0300

    better reqs

--- config/python.py
+++ config/python.py
@@ -2,12 +2,13 @@
 
 import config.shared
 
-scripts: dict[str,str] = {
-    "pybookmarks": "pybookmarks.main:main",
-}
 build_requires: list[str] = config.shared.PBUILD
 test_requires: list[str] = config.shared.PTEST
 types_requires: list[str] = [
     "types-requests",
 ]
 requires = build_requires + test_requires + types_requires
+
+scripts: dict[str,str] = {
+    "pybookmarks": "pybookmarks.main:main",
+}
